8844712d34285c6aee7b6005c46881fcd2c44c89
dayla Kim <ovoa.ovoz@gmail.com>
2022-02-10 18:07:11 +0900

    220210

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (e) => {
+    setKeyword(e.target.value);
+  };
+  useEffect(() => {
+    console.log("call the api!");
+  }, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("I run when 'keyword' changes", keyword);
+    }
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes", counter);
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when 'keyword'&'counter' changes", counter);
+  }, [keyword, counter]);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"} />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      ></input>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click</button>
     </div>
   );
 }
