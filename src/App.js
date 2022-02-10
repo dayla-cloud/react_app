5b6425044bd3c8d80b43d5861fc47702a3fce4de
dayla Kim <ovoa.ovoz@gmail.com>
2022-02-10 18:47:47 +0900

    220210 finish theory

--- src/App.js
+++ src/App.js
@@ -1,36 +1,20 @@
 import { useEffect, useState } from "react";
-
-function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setCounter((prev) => prev + 1);
-  const onChange = (e) => {
-    setKeyword(e.target.value);
-  };
+function Hello() {
   useEffect(() => {
-    console.log("call the api!");
+    console.log("created :D");
+    return () => console.log("destroyed :(");
   }, []);
-  useEffect(() => {
-    if (keyword !== "" && keyword.length > 5) {
-      console.log("I run when 'keyword' changes", keyword);
-    }
-  }, [keyword]);
-  useEffect(() => {
-    console.log("I run when 'counter' changes", counter);
-  }, [counter]);
-  useEffect(() => {
-    console.log("I run when 'keyword'&'counter' changes", counter);
-  }, [keyword, counter]);
+  return <h1>Hello</h1>;
+}
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => {
+    setShowing((prev) => !prev);
+  };
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      ></input>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "hide" : "show"}</button>
     </div>
   );
 }
